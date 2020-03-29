4571089407128fe1ba75f72526350360aeaf5a08
to.mba <to.mba@AMBA11M13.local>
2020-03-30 01:29:13 +0900

    add Area.ts

---
+++ src/library/Area.ts
@@ -0,0 +1,23 @@
+
+enum Area {
+  RB,
+  RT,
+  LB,
+  LT,
+};
+
+namespace Area {
+
+  const all = [
+    Area.RB,
+    Area.RT,
+    Area.LB,
+    Area.LT,
+  ]
+
+  export const admits = (a: any): a is Area => {
+    return all.includes(a)
+  }
+}
+
+export default Area;
