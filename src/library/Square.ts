efb1ecdf297d57a7106c86dff721298820d67a81
to.mba <to.mba@AMBA11M13.local>
2020-03-29 15:55:56 +0900

    add Square.ts

--- src/library/Square.ts
+++ src/library/Square.ts
@@ -1,35 +1,42 @@
 
-// import TetriminoType from '@/libraries/TetriminoType'
+import Direction from '@/foundation/Direction';
+import Limb from './Limb';
+
+// type Square = {
+//   left: boolean;
+//   top: boolean;
+//   right: boolean;
+//   bottom: boolean;
+// };
+
 
 type Square = {
-  left: boolean;
-  top: boolean;
-  right: boolean;
-  bottom: boolean;
+  side: Direction;
+  limb: Limb;
 };
 
 namespace Square {
-  const _admits = (a: {
-    left: boolean;
-    top: boolean;
-    right: boolean;
-    bottom: boolean;
-  }): a is Square => {
-    const some = a.left || a.top || a.right || a.bottom;
-    return some;
+
+  const _admits = (a: any): a is Square => {
+    if (!Direction.admits(a.direction)) return false
+    if (!Limb.admits(a.limb)) return false
+    return true
   };
 
-  export const from = (
-    left: boolean,
-    top: boolean,
-    right: boolean,
-    bottom: boolean
-  ): Square => {
-    const square = { left, top, right, bottom };
+  export const from = (side: Direction, limb: Limb): Square => {
+    const square = { side, limb };
     if (_admits(square)) return square;
     // throw new ApplicationError(`Failed to create a square from: ${ square }`)
     throw new Error(`Failed to create a square from: ${square}`);
   };
+}
+
+namespace Square {
+
+  // export const asBlank = (): Square => {
+
+  //   return from(false, false, false, false)
+  // }
 }
 
 export default Square;
