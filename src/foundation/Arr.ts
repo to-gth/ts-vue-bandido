23fea38a0211f7ea5f9b91da995ee7ea7898ce76
to.mba <to.mba@AMBA11M13.local>
2020-03-30 12:26:36 +0900

    add 2 methods to Arr.ts

--- src/foundation/Arr.ts
+++ src/foundation/Arr.ts
@@ -1,9 +1,23 @@
+import Int from 'ts-number/src/Int'
+import Point from './Point'
 
 export namespace Arr {
 
   export const isRectangle = (arr: any[][]): boolean => {
     const set = new Set(arr.map(row => row.length))
     return set.size <= 1
+  }
+
+  export const rectangleOf = (width: Int, height: Int): any[][] => {
+    return [...Array(height)].map(() => [...Array(width)])
+  }
+
+  export const pastedAt = (leftTop: Point, base: any[][], rect: any[][]): any[][] => {
+    const { left, top } = leftTop
+    rect.forEach((row, i) => {
+      base[top + i].splice(left, row.length, ...row)
+    })
+    return base
   }
 }
 
