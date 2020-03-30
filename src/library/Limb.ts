68e6ad8d83381188c299cf7cf15332bab298c321
to.mba <to.mba@AMBA11M13.local>
2020-03-30 12:28:40 +0900

    add directionsOf() to Limb.ts

--- src/library/Limb.ts
+++ src/library/Limb.ts
@@ -1,3 +1,4 @@
+import Direction from '@/foundation/Direction'
 
 enum Limb {
   L,
@@ -24,6 +25,45 @@ namespace Limb {
   export const admits = (a: any): a is Limb => {
 
     return  all.includes(a)
+  }
+}
+
+namespace Limb {
+
+  const hasL = (limb: Limb): boolean => {
+    switch (limb) {
+      case Limb.H:
+      case Limb.R:
+      case Limb.HR: return false
+      default: return true
+    }
+  }
+  const hasH = (limb: Limb): boolean => {
+    switch (limb) {
+      case Limb.L:
+      case Limb.R:
+      case Limb.LR: return false
+      default: return true
+    }
+  }
+  const hasR = (limb: Limb): boolean => {
+    switch (limb) {
+      case Limb.L:
+      case Limb.H:
+      case Limb.LH: return false
+      default: return true
+    }
+  }
+
+  const _directionOfLIn = (side: Direction): Direction => Direction.anticlockwised(side)
+  const _directionOfHIn = (side: Direction): Direction => Direction.oppositeOf(side)
+  const _directionOfRIn = (side: Direction): Direction => Direction.clockwised(side)
+
+  export const directionsOf = (limb: Limb, side: Direction): Direction[] => {
+    const directions: Direction[] = []
+    if (hasL(limb)) directions.push(_directionOfLIn(side))
+    if (hasH(limb)) directions.push(_directionOfHIn(side))
+    if (hasR(limb)) directions.push(_directionOfRIn(side))
   }
 }
 
