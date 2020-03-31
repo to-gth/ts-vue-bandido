aa26ea2b6fa50bee2a1abf66c4960fe4b32b6fcb
to-gth <to.code01.g@gmail.com>
2020-04-01 03:23:10 +0900

    BlankRestrict.whichFor()

--- src/library/BlankRestrict.ts
+++ src/library/BlankRestrict.ts
@@ -1,3 +1,5 @@
+import SquareFilled from './SquareFilled'
+import LimbDirection from './LimbDirection'
 
 enum BlankRestrict {
   Free,
@@ -15,6 +17,16 @@ namespace BlankRestrict {
 
   export const accepts = (a: any): a is BlankRestrict => {
     return all.includes(a)
+  }
+}
+
+namespace BlankRestrict {
+
+  export const whichFor = (direction: LimbDirection, center: SquareFilled): BlankRestrict => {
+
+    return LimbDirection.isIncludedIn(center, direction)
+      ? BlankRestrict.Open
+      : BlankRestrict.Close
   }
 }
 
