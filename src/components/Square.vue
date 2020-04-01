2b86d740559c42b3b0dfe8384f57f74ade203eb0
to-gth <to.code01.g@gmail.com>
2020-04-02 01:44:48 +0900

    tmp

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="square">
+  <div class="square" :class='[side, limb]'>
     <div
       class="bit"
       v-for="(classname, i) of classnames"
       :key="i"
-      :class="clasname"
+      :class="classname"
     />
     <!-- <div class="bit blank"></div>
     <div class="bit fill"></div>
@@ -25,6 +25,8 @@ import LimbDirection from '../library/LimbDirection';
 import Direction from '../foundation/Direction';
 import BlankRestrict from '../library/BlankRestrict';
 import SquareBlank from '../library/SquareBlank';
+import Limb from '../library/Limb';
+import Limbor from '../library/Limbor';
 
 export default Vue.extend({
   name: "Square",
@@ -36,40 +38,65 @@ export default Vue.extend({
     isFilled(): boolean {
       return SquareFilled.accepts(this.square)
     },
-    classNamesOfFilled(): string[] {
-      const filled = this.square
-      const classnames = [...Array(9)].fill('wall')
-      if (filled.deadlocks) classnames[4] = 'deadlock'
-      if (LimbDirection.isIncludedIn(filled, Direction.Up)) classnames[1] = 'passage'
-      if (LimbDirection.isIncludedIn(filled, Direction.Left)) classnames[3] = 'passage'
-      if (LimbDirection.isIncludedIn(filled, Direction.Right)) classnames[5] = 'passage'
-      if (LimbDirection.isIncludedIn(filled, Direction.Down)) classnames[7] = 'passage'
-      return classnames
+    isBlank(): boolean {
+      return !this.isFilled
     },
-    classNamesOfBlank(): string[] {
-      const blank = this.square
-      const classnames = [...Array(9)].fill('unfilled')
-      classnames[1] = this.classnameOf(Direction.Up, blank)
-      classnames[3] = this.classnameOf(Direction.Left, blank)
-      classnames[5] = this.classnameOf(Direction.Right, blank)
-      classnames[7] = this.classnameOf(Direction.Down, blank)
-      return classnames
+    side(): string {
+      if (this.isFilled) return  ''
+      return 'side-' + LimbDirection.nameFor(this.square.side).toLowerCase()
     },
-    classNames(): string[] {
-      return this.isFilled
-        ? this.classNamesOfFilled
-        : this.classNamesOfBlank
+    limb(): string {
+      const names = Limbor.namesFrom(this.square.limb)
+      return names.map((name) => 'limb-' + name).join(' ')
     },
+    // isFilled(): boolean {
+    //   return SquareFilled.accepts(this.square)
+    // },
+    // classnamesOfFilled(): string[] {
+    //   const filled = this.square
+
+    //   const classnames = [...Array(9)].fill('wall')
+    //   classnames[4] = filled.deadlocks ? 'deadlock' : 'passage'
+    //   if (LimbDirection.isIncludedIn(filled, Direction.Up)) classnames[1] = 'passage'
+    //   if (LimbDirection.isIncludedIn(filled, Direction.Left)) classnames[3] = 'passage'
+    //   if (LimbDirection.isIncludedIn(filled, Direction.Right)) classnames[5] = 'passage'
+    //   if (LimbDirection.isIncludedIn(filled, Direction.Down)) classnames[7] = 'passage'
+
+    //   // const index = this.indexOfCorridorOf(filled.side)
+    //   // classnames[index] = 'passage'
+    //   return classnames
+    // },
+    // classnamesOfBlank(): string[] {
+    //   const blank = this.square
+    //   const classnames = [...Array(9)].fill('unfilled')
+    //   classnames[1] = this.classnameOf(Direction.Up, blank)
+    //   classnames[3] = this.classnameOf(Direction.Left, blank)
+    //   classnames[5] = this.classnameOf(Direction.Right, blank)
+    //   classnames[7] = this.classnameOf(Direction.Down, blank)
+    //   return classnames
+    // },
+    // classnames(): string[] {
+    //   return this.isFilled
+    //     ? this.classnamesOfFilled
+    //     : this.classnamesOfBlank
+    // },
   },
   methods: {
     classnameOf(direction: Direction, blank: SquareBlank) {
       const restrict = BlankRestrict.on(direction, blank)
       return BlankRestrict[restrict].toLowerCase()
-    }
+    },
+    // indexOfCorridorOf(side: Direction): number {
+    //   switch (side) {
+    //     case Direction.Up: return 7
+    //     case Direction.Left: return 5
+    //     case Direction.Right: return 3
+    //     case Direction.Down : return 1
+    //     default: throw new ApplicationError(`Failed to get index-of-corridor from: ${side}`)
+    //   }
+    // },
   },
-  mounted() {
-    console.log("square", this.square);
-  }
+  // mounted() {}
 });
 </script>
 
@@ -81,6 +108,9 @@ export default Vue.extend({
   height: 100%;
   /* grid-auto-rows: 1fr; */
   /* grid-auto-rows: 30px; */
+  border: lightgrey 1px solid;
+  margin-top: -1px;
+  margin-left: -1px;
 }
 .bit {
   margin: 0;
@@ -89,10 +119,30 @@ export default Vue.extend({
   /* margin-left: -1px; */
   padding-bottom: 100%;
 }
-.blank {
+
+.wall {
+  background-color: brown;
+}
+.deadlock {
+  background-color: orangered;
+}
+.unfilled {
   background-color: whitesmoke;
 }
-.fill {
-  background-color: steelblue;
+.free {
+  background-color: whitesmoke;
+}
+.open {
+  background-color: orange;
+}
+.close {
+  background-color: grey;
+}
+.side-left .bit:nth-child(6),
+.side-top .bit:nth-child(8),
+.side-right .bit:nth-child(4),
+.side-bottom .bit:nth-child(2),
+.passage {
+  background-color: yellow;
 }
 </style>
