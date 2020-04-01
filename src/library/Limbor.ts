7d868ae9e5107f3a14ab2250c350b1adda6732c5
to-gth <to.code01.g@gmail.com>
2020-04-02 05:57:36 +0900

    append Limbor.D

--- src/library/Limbor.ts
+++ src/library/Limbor.ts
@@ -4,6 +4,7 @@ enum Limbor {
   L = 1 << 0,
   H = 1 << 1,
   R = 1 << 2,
+  D = 1 << 3,
 }
 
 namespace Limbor {
@@ -12,6 +13,7 @@ namespace Limbor {
     Limbor.L,
     Limbor.H,
     Limbor.R,
+    Limbor.D,
   ]
 
   export const isIncludedIn = (limb: Limb, limbor: Limbor): boolean => {
@@ -26,6 +28,7 @@ namespace Limbor {
     if (limb & Limbor.L) names.push(Limbor[Limbor.L])
     if (limb & Limbor.H) names.push(Limbor[Limbor.H])
     if (limb & Limbor.R) names.push(Limbor[Limbor.R])
+    if (limb & Limbor.D) names.push(Limbor[Limbor.D])
     return names
   }
 }
