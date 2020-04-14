0b9e4bc15c7373eae6fd50875d30b0e149a397f5
to-gth <to.code01.g@gmail.com>
2020-04-14 15:50:23 +0900

    updateHand

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,17 +6,33 @@ import CardType from '@/library/CardType'
 Vue.use(Vuex)
 
 const deck = CardType.randomAll()
-const hands = deck.splice(0, 3)
+const hands = deck.splice(0, 5)
 
 export default new Vuex.Store({
   state: {
     deck: deck,
     hands: hands,
+    hand: 0,
     rows: Rows.initialized()
   },
   mutations: {
+
+    UPDATE_HAND: (state, hand) => {
+      state.hand = hand
+    },
   },
   actions: {
+    updateHand: ({ commit }, hand) => {
+      if (hand < 0) return
+      if (hand >= 5) return
+      commit('UPDATE_HAND', hand)
+    },
+    handPrev: ({ dispatch, state }) => {
+      dispatch('updateHand', state.hand - 1)
+    },
+    handNext: ({ dispatch, state }) => {
+      dispatch('updateHand', state.hand + 1)
+    },
   },
   modules: {
   }
